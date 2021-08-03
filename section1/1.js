77b529f7419f6e6554cf7c873a78a09b2ff503e8
Joon Sik Yang <joonsikyang@gmail.com>
2021-08-04 00:26:28 +0900

    Update 1.js

--- section1/1.js
+++ section1/1.js
@@ -18,12 +18,14 @@ function solution(a, b, c) {
 }
 
 // 문제 풀이
-// function solution(a, b, c){
-//   let answer;
-//   if(a<b) answer=a;
-//   else answer=b;
-//   if(c<answer) answer=c; 
-//   return answer;
-// }
+function solution2(a, b, c) {
+    let answer;
+    
+    if(a < b) answer = a;
+    else answer = b;
+    if(c < answer) answer = c;
+
+    return answer;
+}
 
 console.log(solution(2, 5, 1)); // 1
