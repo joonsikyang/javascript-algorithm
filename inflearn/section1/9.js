72372c64655eb40e5da94fe1fe7ca055b5c8993f
Joon Sik Yang <joonsikyang@Joonui-MacBookAir.local>
2021-08-05 20:11:46 +0900

    section 1-9

--- inflearn/section1/9.js
+++ inflearn/section1/9.js
@@ -1,1 +1,26 @@
 // 9. A를 #으로
+
+/**
+ * 대문자로 이루어진 영어 단어가 입력되면 단어에 포함된 ‘A'를 모두 ’#‘으로 바꾸어 출력하는 프로그램
+ * 입력 설명 : 첫 번째 줄에 문자열이 입력된다.
+ * 출력 설명 : 첫 번째 줄에 바뀐 단어를 출력한다.
+ * 입력 예시 : BANANA >>> B#N#N#
+ */
+
+function solution(str) {
+    return str.replace(/A/g, "#")
+}
+
+console.log(solution("BANANA")); // B#N#N#
+
+// 문제 풀이
+function solution2 (s) {
+    let answer = "";
+    
+    for(let x of s){
+        if( x == 'A') answer += '#';
+        else answer += x;
+    }
+
+    return answer;
+}
