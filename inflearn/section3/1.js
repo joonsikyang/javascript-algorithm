8e1696d6a7ff1a5d723d49844289f784f642f886
Joon Sik Yang <joonsikyang@Joonui-MacBookAir.local>
2021-08-15 17:49:22 +0900

    section 3 folder settings. section 3-1

---
+++ inflearn/section3/1.js
@@ -0,0 +1,29 @@
+// 1. 회문 문자열
+
+/**
+ * 회문 문자열 : 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열
+ * 문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES", 회문 문자열이 아니면 “NO"를 출력 하는 프로그램을 작성하세요.
+ * 단 회문을 검사할 때 대소문자를 구분하지 않습니다.
+ */
+
+function solution(str){
+    let answer = 'YES';
+    let upperCaseStr = str.toUpperCase();
+    for(let i = 0; i < (str.length / 2); i++) {
+        if(upperCaseStr[i] !== upperCaseStr[str.length - i - 1]) answer = 'NO';
+    }
+    return answer;
+}
+
+console.log(solution("baaB")); // YES
+
+// 문제 풀이
+function solution(s) {
+    let answer = "YES";
+    s = s.toLowerCase();
+    let len = s.length;
+    for(let i = 0; i < Math.floor(len/2); i++){ // 꼭 내림을 할 필요가 있나? 어짜피 작은데.
+        if(s[i]!= s[len - i - 1]) return "NO";
+    }
+    return answer;
+}
