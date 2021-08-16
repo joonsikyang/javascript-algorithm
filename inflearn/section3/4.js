a97e2781a1f9b942a48e19dfe09ae01f9a307d64
Joon Sik Yang <joonsikyang@Joonui-MacBookAir.local>
2021-08-17 00:40:39 +0900

    section 3-5 DONE, section 3-4 WIP

--- inflearn/section3/4.js
+++ inflearn/section3/4.js
@@ -1,1 +1,27 @@
 // 4. 가장 짧은 문자거리
+
+/**
+ * 한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램을 작성하세요.
+ * 입력 설명 : 첫 번째 줄에 문자열 s와 문자 t가 주어진다. 문자열과 문자는 소문자로만 주어집니다.
+ * 출력 설명 : 첫 번째 줄에 각 문자열 s의 각 문자가 문자 t와 떨어진 거리를 순서대로 출력한다.
+ * 입력 예제 : "teachermode", "e" >>> 10121012210
+ */
+
+function solution(str, char) {
+    let answer = [];
+    let strArr = str.split("");
+    for (let i in strArr) {
+        // 문자열과 동일한 경우 >>> 0
+        if(strArr[i] === char) answer.push(0);
+
+        // 문자열과 동일하지 않은 경우 >>> index 값의 차이 (최소값)
+    }
+
+    return answer.join('')
+}
+
+// 주어진 문자의 index를 구해서 a / b / c
+// 특정 문자 index 와의 차이값 중 최소값
+
+
+console.log(solution("teachermode", "e")); 
--- inflearn/section3/5.js
+++ inflearn/section3/5.js
@@ -1,1 +1,45 @@
 // 5. 문자열 압축
+
+/**
+ * 알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우
+ * 반복되는 문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하시오. 
+ * 단 반복횟수가 1인 경우 생략합니다.
+ * 입력 설명 : 첫 줄에 문자열이 주어진다. 문자열의 길이는 100을 넘지 않는다.
+ * 출력 설명 : 첫 줄에 압축된 문자열을 출력한다.
+ * 입력 예제 : KKHSSSSSSSE >>> K2HS7E
+ */
+
+function solution(str) {
+    let answer = str[0];
+    let count = 1;
+
+    for(let i = 1; i < str.length; i++) {
+        if (str[i - 1] === str[i]) {
+            count++;
+            answer = count > 2 ? answer.slice(0, -1) + count : answer + count;
+        } else {
+            answer += str[i];
+            count = 1;
+        }
+    }
+
+    return answer;
+}
+
+console.log(solution("KKHSSSSSSSE"));
+
+// 문제 풀이
+function solution2(s) {
+    let answer = "";
+    let cnt = 1;
+    s = s + " ";
+    for(let i = 0; i < s.length - 1; i++) {
+        if(s[i] === s[i+1]) cnt++;
+        else {
+            answer += s[i];
+            if (cnt > 1) answer += String(cnt);
+            cnt = 1;
+        }
+    }
+    return answer;
+}
